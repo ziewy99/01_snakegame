d2c33e51c33fd4d4bdd3478a1393577e84264c77
ziewy99 <wyz517619103@gmail.com>
2025-06-24 14:20:35 +0900

    update

---
+++ 制作/Assets/Script/Body.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Body : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator animator;
+    public Vector2 moveDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        UpdateAnimation();
+    }
+
+    void UpdateAnimation()
+    {
+        if (moveDirection == Vector2.right) animator.Play("Walk_right");
+        else if (moveDirection == Vector2.left) animator.Play("Walk_left");
+        else if (moveDirection == Vector2.up) animator.Play("Walk_up");
+        else if (moveDirection == Vector2.down) animator.Play("Walk_down");
+    }
+    public void SetDirection(Vector2 direction)
+    {
+        moveDirection = direction;
+    }
+}
