b3ebf6b9fd370179a422299930c1df2cf0654b3a
ziewy99 <wyz517619103@gmail.com>
2025-07-03 23:02:36 +0900

    upload

---
+++ 制作/Assets/Script/ButtonController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void LoadTitleScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
---
+++ 制作/Assets/Script/ResultManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class ResultManager : MonoBehaviour
+{
+    public TextMeshProUGUI resultScoreText;
+
+    void Start()
+    {
+        Player player = GameObject.Find("Player").GetComponent<Player>();
+        if (player != null && resultScoreText != null)
+        {
+            int score = player.score;
+            Debug.Log("score");
+            resultScoreText.text = "Score: " + score.ToString();
+        }
+    }
+}
